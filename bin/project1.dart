0660700cc3dfbfcd172193cc4f8744a9d8a35d26
nasi321 <nasinasri6102@gmail.com>
2023-10-27 09:53:27 +0530

    initial commit

--- bin/project1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:project1/project1.dart' as project1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${project1.calculate()}!');
-}
---
+++ bin/variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = "Fathima Nasrin";
+  int age = 20;
+  String email = 'nasinasri6102@gmail.com';
+  int phone = 7907416102;
+  double cgpa = 8;
+  int a = 10, b = 20;
+
+  print(name);
+  print('My name is $name');
+  print('I am $age old');
+  print("My email id is $email");
+  print('My phone number is $phone');
+  print('cgpa is $cgpa');
+  print("total sum = ${a + b}");
+}
